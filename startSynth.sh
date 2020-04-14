7c2061f58ee0081d2651497e05d7d1c95507091e
Bill Staffeldt <will.staf@gmail.com>
2020-04-14 15:59:51 -0400

    Update startSynth.sh

--- startSynth.sh
+++ startSynth.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 jackd -d alsa --device hw:0 --rate 44100 --period 1024 &
 sleep 3
 sonic-pi-tool.py start-server --path /opt/sonic-pi-3.2.2/ &
 sleep 23
-sonic-pi-tool.py run-file midiSynth.rb
+sonic-pi-tool.py run-file $DIR/midiSynth.rb
