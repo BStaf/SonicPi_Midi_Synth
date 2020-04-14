972e4e9d569fcdc99c69920893095e03ada00930
Bill Staffeldt <will.staf@gmail.com>
2020-04-14 15:35:47 -0400

    Update installSonicPi.sh

--- installSonicPi.sh
+++ installSonicPi.sh
@@ -4,3 +4,13 @@ sudo rpi-update
 sudo apt install -y ruby
 sudo apt install -y ./sonic-pi-3.2.2_1.armhf.deb
 
+## Script Uses the sonic-pi-tool python port
+## Get whats needed to run script
+# Install dependencies:
+pip install oscpy click
+# Download script:
+curl -O https://raw.githubusercontent.com/emlyn/sonic-pi-tool/master/sonic-pi-tool.py
+# Make it executable:
+chmod +x sonic-pi-tool.py
+# Copy it somewhere on the PATH:
+sudo cp sonic-pi-tool.py /usr/local/bin/
