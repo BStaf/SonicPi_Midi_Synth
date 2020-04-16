a539838b6125dcd749717175e3ee346e1d45caec
BillS <will.staf@gmail.com>
2020-04-16 15:47:48 -0400

    altered start script

--- startSynth.sh
+++ startSynth.sh
@@ -2,7 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-jackd -d alsa --device hw:0 --rate 44100 --period 1024 &
+cd $DIR
+#Get latest version
+git pull
+
+jackd -d alsa --device hw:3 --rate 44100 --period 512 &
 sleep 3
 sonic-pi-tool.py start-server --path /opt/sonic-pi-3.2.2/ &
 sleep 23
